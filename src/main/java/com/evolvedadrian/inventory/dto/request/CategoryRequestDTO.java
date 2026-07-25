2c79cde870885473aac41feec6bcec124cdff5a9
HelloAdrian2k <mz.adriaan@gmail.com>
2026-07-25 21:23:07 +0200

    feat: add category DTOs

---
+++ src/main/java/com/evolvedadrian/inventory/dto/request/CategoryRequestDTO.java
@@ -0,0 +1,13 @@
+package com.evolvedadrian.inventory.dto.request;
+
+public class CategoryRequestDTO {
+    private String name;
+
+    public String getName() {
+        return this.name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
---
+++ src/main/java/com/evolvedadrian/inventory/dto/response/CategoryResponseDTO.java
@@ -0,0 +1,23 @@
+package com.evolvedadrian.inventory.dto.response;
+
+public class CategoryResponseDTO {
+    private Integer id;
+
+    private String name;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
