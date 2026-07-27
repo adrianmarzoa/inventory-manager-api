a738dbb683a75dea2006667a803c146553a8ece0
HelloAdrian2k <mz.adriaan@gmail.com>
2026-07-27 18:30:47 +0200

    feat: add warehouse DTOs

---
+++ src/main/java/com/evolvedadrian/inventory/dto/request/WarehouseRequestDTO.java
@@ -0,0 +1,23 @@
+package com.evolvedadrian.inventory.dto.request;
+
+public class WarehouseRequestDTO {
+    private String name;
+
+    private String location;
+
+    public String getName() {
+        return this.name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getLocation() {
+        return this.location;
+    }
+
+    public void setLocation(String location) {
+        this.location = location;
+    }
+}
---
+++ src/main/java/com/evolvedadrian/inventory/dto/response/WarehouseResponseDTO.java
@@ -0,0 +1,35 @@
+package com.evolvedadrian.inventory.dto.response;
+
+import jakarta.persistence.Column;
+
+public class WarehouseResponseDTO {
+    private Integer id;
+
+    private String name;
+
+    private String location;
+
+    public Integer getId() {
+        return this.id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getLocation() {
+        return this.location;
+    }
+
+    public void setLocation(String location) {
+        this.location = location;
+    }
+}
