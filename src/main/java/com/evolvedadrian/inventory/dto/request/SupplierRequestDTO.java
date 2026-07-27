3ba1ab1e5b6320f9fd9a319324b84dfec9ce105f
HelloAdrian2k <mz.adriaan@gmail.com>
2026-07-27 18:30:40 +0200

    feat: add supplier DTOs

---
+++ src/main/java/com/evolvedadrian/inventory/dto/request/SupplierRequestDTO.java
@@ -0,0 +1,35 @@
+package com.evolvedadrian.inventory.dto.request;
+
+import jakarta.persistence.Column;
+
+public class SupplierRequestDTO {
+    private String name;
+
+    private String email;
+
+    private String phone;
+
+    public String getEmail() {
+        return this.email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPhone() {
+        return this.phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+}
