961bd278a92337c429fcb35ff9c0661b42d79066
HelloAdrian2k <mz.adriaan@gmail.com>
2026-08-04 17:26:07 +0200

    feat: add supplier mapper

---
+++ src/main/java/com/evolvedadrian/inventory/mapper/SupplierMapper.java
@@ -0,0 +1,29 @@
+package com.evolvedadrian.inventory.mapper;
+
+import com.evolvedadrian.inventory.dto.request.SupplierRequestDTO;
+import com.evolvedadrian.inventory.dto.response.SupplierResponseDTO;
+import com.evolvedadrian.inventory.entity.Supplier;
+import org.springframework.stereotype.Component;
+
+@Component
+public class SupplierMapper {
+    public Supplier toEntity(SupplierRequestDTO dto){
+        Supplier supplier = new Supplier();
+
+        supplier.setName(dto.getName());
+        supplier.setEmail(dto.getEmail());
+        supplier.setPhone(dto.getPhone());
+
+        return supplier;
+    }
+    public SupplierResponseDTO toResponse(Supplier supplier){
+        SupplierResponseDTO dto = new SupplierResponseDTO();
+
+        dto.setId(supplier.getId());
+        dto.setName(supplier.getName());
+        dto.setEmail(supplier.getEmail());
+        dto.setPhone(supplier.getPhone());
+
+        return dto;
+    }
+}
