75f489964269faefd203e32307a2d724d14002ce
HelloAdrian2k <mz.adriaan@gmail.com>
2026-08-05 22:07:53 +0200

    feat: add api error handling for validation and generic exceptions

--- src/main/java/com/evolvedadrian/inventory/exception/ApiErrorHandler.java
+++ src/main/java/com/evolvedadrian/inventory/exception/ApiErrorHandler.java
@@ -3,13 +3,17 @@ package com.evolvedadrian.inventory.exception;
 import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+
+import java.util.HashMap;
+import java.util.Map;
 
 @ControllerAdvice
 public class ApiErrorHandler {
     @ExceptionHandler(ResourceNotFoundException.class)
-    public ResponseEntity<ApiError> handleResourceNotFound(ResourceNotFoundException ex, HttpServletRequest request) {
+    public ResponseEntity<ApiError> handleNotFound(ResourceNotFoundException ex, HttpServletRequest request) {
         HttpStatus status = HttpStatus.NOT_FOUND;
 
         ApiError apiError = new ApiError(
@@ -23,13 +27,36 @@ public class ApiErrorHandler {
     }
 
     @ExceptionHandler(DuplicatedResourceException.class)
-    public ResponseEntity<ApiError> handleDuplicatedResource(DuplicatedResourceException ex, HttpServletRequest request){
+    public ResponseEntity<ApiError> handleDuplicated(DuplicatedResourceException ex, HttpServletRequest request){
         HttpStatus status = HttpStatus.CONFLICT;
 
         ApiError apiError = new ApiError(
                 status.value(),
                 status.getReasonPhrase(),
                 ex.getMessage(),
+                request.getRequestURI()
+        );
+
+        return ResponseEntity.status(status).body(apiError);
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<Map<String,String>> handleValidation(MethodArgumentNotValidException ex){
+        Map<String, String> apiError = new HashMap<>();
+
+        ex.getBindingResult().getFieldErrors().forEach(fieldError -> apiError.put(fieldError.getField(), fieldError.getDefaultMessage()));
+
+        return ResponseEntity.badRequest().body(apiError);
+    }
+
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<ApiError> handleGeneric(Exception ex, HttpServletRequest request){
+        HttpStatus status = HttpStatus.INTERNAL_SERVER_ERROR;
+
+        ApiError apiError = new ApiError(
+                status.value(),
+                status.getReasonPhrase(),
+                "Unexpected server error",
                 request.getRequestURI()
         );
 
